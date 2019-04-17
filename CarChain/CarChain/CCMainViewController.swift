870dbee83c3faef11c33bc5afdb1cf901fa71ea2
Pablo Blanco Peris <pablo.blanco@ext.universia.net>
2019-04-17 11:24:27 +0200

    conexión con smartcontract

--- CarChain/CarChain/CCMainViewController.swift
+++ CarChain/CarChain/CCMainViewController.swift
@@ -25,13 +25,17 @@ class CCMainViewController: UIViewController {
     
     let locationManager = CLLocationManager()
     let geocoder = CLGeocoder()
-    let web3 = Web3(rpcURL: "https://rinkeby.infura.io/v3/911ffdd646fe4561820ce1c146be7250")
-    let contractAddress = try! EthereumAddress(hex: "0x526F5F06D6f1f9e63c6f344Ab5b160DE7b1eaCEa", eip55: true)
-    let managerAddress = try! EthereumAddress(hex: "0x711bb2cDfA7f6d3C2D4d2dd167E45D80A4Af1EfD", eip55: false)
     
+    
+    let managerAddress = try! EthereumAddress(hex: "0x711bb2cDfA7f6d3C2D4d2dd167E45D80A4Af1EfD", eip55: false)
     //        let contractAddress = try! EthereumAddress(hex: "0x711bb2cDfA7f6d3C2D4d2dd167E45D80A4Af1EfD", eip55: true)
     let contractJsonABI = "[{\"constant\":false,\"inputs\":[{\"name\":\"_id\",\"type\":\"uint256\"},{\"name\":\"_credit\",\"type\":\"uint256\"}],\"name\":\"registerNewUser\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"name\":\"users\",\"outputs\":[{\"name\":\"id\",\"type\":\"uint256\"},{\"name\":\"credit\",\"type\":\"uint256\"},{\"name\":\"carId\",\"type\":\"uint256\"},{\"name\":\"registered\",\"type\":\"bool\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_id\",\"type\":\"uint256\"},{\"name\":\"_plate\",\"type\":\"string\"}],\"name\":\"registerNewCar\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"_id\",\"type\":\"uint256\"}],\"name\":\"getUserCredit\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_carId\",\"type\":\"uint256\"},{\"name\":\"_userId\",\"type\":\"uint256\"}],\"name\":\"rentCar\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_carId\",\"type\":\"uint256\"},{\"name\":\"_userId\",\"type\":\"uint256\"}],\"name\":\"returnCar\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"name\":\"cars\",\"outputs\":[{\"name\":\"id\",\"type\":\"uint256\"},{\"name\":\"plate\",\"type\":\"string\"},{\"name\":\"userId\",\"type\":\"uint256\"},{\"name\":\"registered\",\"type\":\"bool\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"minimumRentCredit\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"}]".data(using: .utf8)!
     
+    
+    let web3 = Web3(rpcURL: "https://rinkeby.infura.io/v3/911ffdd646fe4561820ce1c146be7250")
+//    let contractAddress = try! EthereumAddress(hex: "0x526F5F06D6f1f9e63c6f344Ab5b160DE7b1eaCEa", eip55: true)
+    let contractAddress = try! EthereumAddress(hex: "0x5a84615d6c15f7ebe72a32c02a4f251948024de9", eip55: false)
+
     
     
     //    let fakeVehicles = [MKAnnotation]()
@@ -55,82 +59,26 @@ class CCMainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        CCVehicleManager.sharedInstance.load()
-//        updatingLocation = false
-//        startLocationManager()
+        CCVehicleManager.sharedInstance.load()
+        updatingLocation = false
+        startLocationManager()
         
-        connectToSmartContract()
-        testContract()
+        
+//        registerNewUser()
+//        getMinimumRentCredit()
+
     }
     
-    func testContract(){
-        
+    func getMinimumRentCredit(){
         let contract = try! web3.eth.Contract(json: contractJsonABI, abiKey: nil, address: contractAddress)
-
         
         firstly {
-            try contract["getUserCredit"]!(EthereumAddress(hex: "0x711bb2cDfA7f6d3C2D4d2dd167E45D80A4Af1EfD", eip55: false), 1).call()
-            }.done { outputs in
-                print(outputs[""] as? BigUInt)
+            contract["users"]!(BigUInt(1)).call()
+            }.done { hash in
+                print(hash)
             }.catch { error in
                 print(error)
         }
-        
-        
-        registerNewUser()
-//        firstly {
-//            contract["minimumRentCredit"]!(managerAddress).call()
-//            }.done { outputs in
-//                print(outputs[""] as? BigUInt!)
-//            }.catch { error in
-//                print(error)
-//        }
-        
-//        firstly {
-//            contract["registerNewCar"]!(managerAddress).call()
-//            }.done { outputs in
-//                print(outputs[""] as? BigUInt!)
-//            }.catch { error in
-//                print(error)
-//        }
-        
-//        let myPrivateKey = try! EthereumPrivateKey(hexPrivateKey: "0F83EC16705DD8071481592816ABCFDF883A173F9293497C4EEDB33FFC6150CB")
-//        var transaction = contract["registerNewCar"]?(managerAddress).createTransaction(nonce: 1, from: myPrivateKey.address, value: 1, gas: 21000, gasPrice: EthereumQuantity(quantity: 21.gwei))
-//
-//
-//        transaction?.data = try! EthereumData(bytes: [1, 2])
-//
-//        var nonc = 0
-//
-//
-//        let signedTx = try! transaction?.sign(with: myPrivateKey, chainId: 4)
-        
-        
-        
-//        firstly {
-//            web3.eth.sendRawTransaction(transaction: signedTx!)
-//            }.done { txHash in
-//                print(txHash)
-//            }.catch { error in
-//                print(error)
-        //        }
-        
-        
-        
-        // Send some tokens to another address (locally signing the transaction)
-        //        let myPrivateKey = try! EthereumPrivateKey(hexPrivateKey: "...")
-        //        guard let transaction = contract["transfer"]?(contractAddress, BigUInt(100000)).createTransaction(nonce: 0, from: (myPrivateKey.address), value: 0, gas: 150000, gasPrice: EthereumQuantity(quantity: 21.gwei)) else {
-        //            return
-        //        }
-        //        let signedTx = try! transaction.sign(with: myPrivateKey)
-        //
-        //        firstly {
-        //            web3.eth.sendRawTransaction(transaction: signedTx)
-        //            }.done { txHash in
-        //                print(txHash)
-        //            }.catch { error in
-        //                print(error)
-        //        }
     }
     
     func registerNewUser(){
@@ -147,9 +95,9 @@ class CCMainViewController: UIViewController {
         
         do {
             
-            let c = contract["registerNewUser"]?(contractAddress, BigUInt(1), BigUInt(100))
+            let c = contract["registerNewUser"]?(BigUInt(1), BigUInt(100))
             let transaction: EthereumTransaction = c!
-                .createTransaction(nonce: 14,
+                .createTransaction(nonce: 17,
                                    from: myPrivateKey.address,
                                    value: 0,
                                    gas: 210000,
@@ -170,17 +118,10 @@ class CCMainViewController: UIViewController {
         }
     }
     
-    
-    func connectToSmartContract(){
-
-        
-    }
-    
-    
     override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//        self.mapMKView.delegate = self
-//        self.mapMKView.addAnnotations(CCVehicleManager.sharedInstance.vehicles)
+        super.viewWillAppear(animated)
+        self.mapMKView.delegate = self
+        self.mapMKView.addAnnotations(CCVehicleManager.sharedInstance.vehicles)
     }
     
     
